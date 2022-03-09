a4c18bdbd69c1931ca361e1ce55abf77e2ab0dd8
juan <juanperu@gmail.com>
2022-03-09 11:15:10 -0500

    Update functions.php

--- src/functions.php
+++ src/functions.php
@@ -195,6 +195,7 @@ function task(string $name, $body = null): Task
     }
 
     $task->saveSourceLocation();
+    $deployer->tasks->set($name, $task);
 
     if (!empty(desc())) {
         $task->desc(desc());
